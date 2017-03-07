76302208f1ae5750ad4f8a0359fac114d2904539
cdelledonne <Carlo@MacBook-Pro-di-Carlo.local>
2017-03-07 21:39:16 +0100

    first DFT version

--- Code/dft_test/src/dft_test.c
+++ Code/dft_test/src/dft_test.c
@@ -30,10 +30,12 @@
 #include "util.h"
 
 
-#define F1              5
-#define F2              10
-#define SAMPL_FREQ      256                         // Simulation time:
-#define N_SAMPLES       256                         // N_SAMPLES/SAMPL_FREQ
+#define F1              2
+#define F2              4
+#define SAMPL_FREQ      32                         // Simulation time:
+#define N_SAMPLES       32                         // N_SAMPLES/SAMPL_FREQ
+//#define SAMPL_PER       0.00390625
+#define SAMPL_PER       0.03125
 
 #define PI              3.1415926536
 
@@ -42,19 +44,31 @@ int fputc(int _c, register FILE *_fp);
 int fputs(const char *_ptr, register FILE *_fp);
 
 
-DSPLIB_DATA(x1,N_SAMPLES)
+DSPLIB_DATA(x1,4)
 _q15 x1[N_SAMPLES];
 
-DSPLIB_DATA(x2,N_SAMPLES)
+DSPLIB_DATA(x2,4)
 _q15 x2[N_SAMPLES];
 
-DSPLIB_DATA(x,N_SAMPLES)
+DSPLIB_DATA(x,4)
 _q15 x[N_SAMPLES];                      // Input signal x = x1 + x2
+
+//DSPLIB_DATA(X_RE,4)
+//_q15 X_RE[N_SAMPLES];                   // Real part of X = DFT(x)
+
+//DSPLIB_DATA(X_IM,4)
+//_q15 X_IM[N_SAMPLES];                   // Imaginary part of X = DFT(x)
+
+//DSPLIB_DATA(X,4)
+//_q15 X[N_SAMPLES];                      // Magnitude of X = DFT(x)
+
+DSPLIB_DATA(temp,4)
+_q15 temp[2*N_SAMPLES];
 
 msp_status status;
 
-float X_RE[N_SAMPLES], X_IM[N_SAMPLES]; // DFT of x (real and imaginary parts)
-float X[N_SAMPLES];                     // spectrum of x
+//float X_RE[N_SAMPLES], X_IM[N_SAMPLES]; // DFT of x (real and imaginary parts)
+//float X[N_SAMPLES];                     // spectrum of x
 
 
 void main(void)
@@ -71,24 +85,25 @@ void main(void)
 
     PM5CTL0 &= ~LOCKLPM5;           // as done by PMM_unlockLPM5()
 
+    P1DIR |= 0x01;
+    P1OUT = 0x00;                   // Turn off LED1
+
     __enable_interrupt();
 
     printf("All set up!\n\r");      // remove when optimising
 
     // Generate two sine waves and store them into x1 and x2
-    // for cosOmega use the value of cos(2*pi*frequency/SAMPL_FREQ)
-    // for sinOmega use the value of sin(2*pi*frequency/SAMPL_FREQ)
 
     msp_sinusoid_q15_params sinParams;
     sinParams.length = N_SAMPLES;
     sinParams.amplitude = _Q15(0.5);
 
-    sinParams.cosOmega = _Q15(cosf(2*PI*F1/SAMPL_FREQ));        // Frequency
-    sinParams.sinOmega = _Q15(sinf(2*PI*F1/SAMPL_FREQ));        // F1 = 5 Hz
+    sinParams.cosOmega = _Q15(cosf(2*PI*F1/SAMPL_FREQ));
+    sinParams.sinOmega = _Q15(sinf(2*PI*F1/SAMPL_FREQ));
     status = msp_sinusoid_q15(&sinParams, x1);
 
-    sinParams.cosOmega = _Q15(cosf(2*PI*F2/SAMPL_FREQ));        // Frequency
-    sinParams.sinOmega = _Q15(sinf(2*PI*F2/SAMPL_FREQ));        // F2 = 10 Hz
+    sinParams.cosOmega = _Q15(cosf(2*PI*F2/SAMPL_FREQ));
+    sinParams.sinOmega = _Q15(sinf(2*PI*F2/SAMPL_FREQ));
     status = msp_sinusoid_q15(&sinParams, x2);
 
     // Input signal x is the sum of two sine waves
@@ -96,7 +111,57 @@ void main(void)
     addParams.length = N_SAMPLES;
     status = msp_add_q15(&addParams, x1, x2, x);
 
+    // Scale down signal x
+    msp_scale_q15_params scaleParams;
+    scaleParams.length = N_SAMPLES;
+    scaleParams.scale = _Q15(SAMPL_PER);
+    scaleParams.shift = 0;
+    status = msp_scale_q15(&scaleParams, x, x);
+
+    // Perform the DFT on signal x
+    /**
+     *  for k = 0:N_SAMPLES
+     *      X_RE[k] = X_IM[k] = 0;
+     *      for n = 0:N_SAMPLES
+     *          X_RE[k] = X_RE[k] + x[n]*cos(2*M_PI*n*k/N_SAMPLES);
+     *          X_IM[k] = X_IM[k] - x[n]*sin(2*M_PI*n*k/N_SAMPLES);
+     *      end for
+     *      X[k] = sqrt(X_RE[k]*X_RE[k] + X_IM[k]*X_IM[k]);
+     *  end for
+     */
+
+    uint16_t k, n;
+    msp_mpy_q15_params mpyParams;
+    mpyParams.length = N_SAMPLES;
+
+    for (k=0; k<N_SAMPLES; k++) {
+
+        for (n=0; n<N_SAMPLES; n++)                     // x1 will contain a
+            x1[n] = _Q15(cosf(n*k*2*PI/N_SAMPLES));     // cos wave at f = k
+
+        for (n=0; n<N_SAMPLES; n++)                     // x2 will contain a
+            x2[n] = _Q15(-sinf(n*k*2*PI/N_SAMPLES));    // sin wave at f = k
+
+        status = msp_mpy_q15(&mpyParams, x, x1, x1);    // x1 = x * cos(...)
+        status = msp_mpy_q15(&mpyParams, x, x2, x2);    // x2 = x * -sin(...)
+
+        temp[k] = temp[k+N_SAMPLES] = 0;
+        for (n=0; n<N_SAMPLES; n++) {
+            temp[k] += x1[n];                           // Real part
+            temp[k+N_SAMPLES] += x2[n];                 // Imaginary part
+        }
+    }
+
+    // Final result x = sqrt(x_RE * x_RE + x_IM * x_IM)
+    status = msp_mpy_q15(&mpyParams, &temp[0], &temp[0], x1);
+    status = msp_mpy_q15(&mpyParams, &temp[N_SAMPLES], &temp[N_SAMPLES], x2);
+    status = msp_add_q15(&addParams, x1, x2, x);
+    for (k=0; k<N_SAMPLES; k++)
+        x[k] = sqrtf(x[k]);
+
     __no_operation();               // breakpoint here to check results
+
+    P1OUT |= 0x01;                  // Turn on LED1
 
     while(1);
 
